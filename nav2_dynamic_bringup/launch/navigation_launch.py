0c05234e8e139c012189b9924d53130dda71a2e4
pravin <behera.pravin@gmail.com>
2025-02-24 00:10:43 +0530

    dynamic bringup

--- nav2_dynamic_bringup/launch/navigation_launch.py
+++ nav2_dynamic_bringup/launch/navigation_launch.py
@@ -15,7 +15,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('nav2_bringup')
+    bringup_dir = get_package_share_directory('nav2_dynamic_bringup')
 
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
